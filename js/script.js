c956af63ae7bd60c74034d13f3f2a464f5c06206
pashariziqroyan <pashariziqroyan28@gmail.com>
2026-09-12 14:24:36 +0700

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const promoButton = document.querySelector("#btn");
+
+promoButton.addEventListener("click", () => {
+  promoButton.textContent = "Promo: Beli 2 gratis tester!";
+  console.log("Promo Kopi Nusa berhasil ditampilkan.");
+});
