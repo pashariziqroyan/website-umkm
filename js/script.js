b5da9d040f6e0c2b6372db5975fbc4a0fdbe7d21
pashariziqroyan <pashariziqroyan28@gmail.com>
2026-09-13 01:05:44 +0700

    Last Section Jam Buka & Lokasi

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ const promoButton = document.querySelector("#btn");
 
 promoButton.addEventListener("click", () => {
   promoButton.textContent = "Promo: Beli 2 gratis 1!";
-  console.log("Promo Kopi Nusa berhasil ditampilkan.");
+  console.log("Promo berhasil ditampilkan.");
 });
