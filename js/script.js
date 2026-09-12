afdfba044ef26ff16383dfcf098968d47ed83648
pashariziqroyan <pashariziqroyan28@gmail.com>
2026-09-12 23:43:31 +0700

    Tambah Section Jam Buka & Lokasi

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#btn");
 
 promoButton.addEventListener("click", () => {
-  promoButton.textContent = "Promo: Beli 2 gratis tester!";
+  promoButton.textContent = "Promo: Beli 2 gratis 1!";
   console.log("Promo Kopi Nusa berhasil ditampilkan.");
 });
